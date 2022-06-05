fabec1917ce8fea6ad85daea0afaaac2ace0b88b
David Grayson <davidegrayson@gmail.com>
2022-06-05 14:03:26 -0700

    macos: Update sdk_builder.sh to fix the redundant 'v1' folder in new SDKs.

--- macos/sdk_builder.sh
+++ macos/sdk_builder.sh
@@ -4,9 +4,14 @@ tar -xf $src
 mv MacOSX*.sdk $out
 
 cd $out
+
 ruby -rjson \
   -e "print JSON.load(File.read('SDKSettings.json')).fetch('Version')" \
   > version.txt
 
 # Make sure the STL headers are in the expected place.
+if [ -d usr/include/c++/v1 ]; then
+  mv usr/include/c++/v1/* usr/include/c++/
+  rmdir usr/include/c++/v1
+fi
 ls usr/include/c++/iterator > /dev/null
