f6243251fa5887829e0e7f10ecb92d031f3b3226
David Grayson <davidegrayson@gmail.com>
2022-09-10 14:31:31 -0700

    openocd: Update to the latest version (fixes a compilation error for macos.arm).

--- pkgs/openocd/default.nix
+++ pkgs/openocd/default.nix
@@ -1,7 +1,7 @@
 { crossenv, libusb }:
 
 let
-  version = "2018-09-19";
+  version = "2022-09-08";
 
   name = "openocd-${version}";
 
@@ -9,8 +9,8 @@ let
 
   src = nixpkgs.fetchgit {
     url = "git://repo.or.cz/openocd";  # official mirror
-    rev = "b2d259f67cc3ee4b689e704228d97943bae94064";
-    sha256 = "126zcgq3dplhzmy28rqp0y0df92xgb2qkh9nfc70mka7jwj994yx";
+    rev = "de99836cf639b63dc357a34c13825669af841f17";
+    sha256 = "sha256-+51wBfDc086Z+GauDeehsrSiYGNHrI8mmyIONR47WYo=";
     fetchSubmodules = true;
   };
 
