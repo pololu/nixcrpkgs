a91a5e8e7212d060129782a233ec0f14a45b52e6
David Grayson <davidegrayson@gmail.com>
2023-01-02 17:28:25 -0800

    libusbp: 1.2.0 -> 1.3.0

--- pkgs/libusbp/default.nix
+++ pkgs/libusbp/default.nix
@@ -1,13 +1,13 @@
 { crossenv, libudev }:
 
 let
-  version = "1.2.0";
+  version = "1.3.0";
 
   name = "libusbp-${version}";
 
   src = crossenv.nixpkgs.fetchurl {
     url = "https://github.com/pololu/libusbp/archive/${version}.tar.gz";
-    sha256 = "15qss392iy01fbxkqkikbsy218bn1b6z21zalbgsld6h8jy7icsj";
+    hash = "sha256-AciAH0HI7JqhvxQlNwRP5XWWCliBjMEBCdFRIuh5YAw=";
   };
 
   lib = crossenv.make_derivation {
